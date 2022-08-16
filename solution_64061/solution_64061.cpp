55c5912996d7202b7a5fa38e1bb21fb4b8a80723
Jaejun Lee <jaejun.lees@gmail.com>
2022-08-16 20:21:36 +0900

    문제 풀이 추가

---
+++ solution_64061/solution_64061.cpp
@@ -0,0 +1,94 @@
+﻿// solution_64061.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    vector<int> pot;
+    vector<vector<int>> b = board;
+    for (int& cur_move : moves)
+    {
+        int cols = -1 + cur_move;
+        int rows = 0;
+        while (b[rows][cols] == 0)
+        {
+            rows++;
+            if (rows == b.size())
+            {
+                rows -= 1;
+                break;
+            }
+                
+        }
+
+        if (b[rows][cols] == 0)
+        {
+            continue;
+        }
+        else
+        {
+            if (pot.empty())
+            {
+                pot.push_back(b[rows][cols]);
+                b[rows][cols] = 0;
+                continue;
+            }
+            else if (pot.back() == b[rows][cols])
+            {
+                pot.pop_back();
+                answer+=2;
+            }
+            else if (pot.back() != b[rows][cols])
+            {
+                pot.push_back(b[rows][cols]);
+            }
+            b[rows][cols] = 0;
+        }
+
+    }
+    return answer;
+}
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    int curr = (int)n;
+    
+    while (curr > 9)
+    {
+        cout << curr % 10 << endl;
+        answer.push_back(curr % 10);
+        curr = curr / 10;
+    }
+    answer.push_back(curr);
+
+    cout << "0--" << endl;
+    for (auto& val : answer)
+    {
+        cout << val << endl;
+    }
+
+    return answer;
+}
+int main()
+{
+    //vector<vector<int>> q1 = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+    //vector<int> moves = { 1, 5, 3, 5, 1, 2, 1, 4 };
+   // int ans = solution(q1, moves);
+   // std::cout << ans << "\n";
+
+    solution(54321);
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
---
+++ solution_67256/solution_67256.cpp
@@ -0,0 +1,106 @@
+﻿// solution_67256.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<int> numbers, string hand) {
+
+    string answer = "";
+
+    int bef_num_right = 12;
+    int bef_num_left = 10;
+
+    //vector<bool> ans_bool; // # true면 L, false면 R로 취급
+
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (i == 0 && numbers[i] == 0)
+        {
+            if (hand == "left")
+                answer.append("L");
+            else if (hand == "right")
+                answer.append("R");
+            continue;
+        }
+
+        if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7)
+        {
+            answer.append("L");
+            bef_num_left = numbers[i];
+        }
+        else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9)
+        {
+            answer.append("R");
+            bef_num_right = numbers[i];
+        }
+        else if (numbers[i] == 2 || numbers[i] == 5 || numbers[i] == 8 || numbers[i] == 0)
+        {
+            if (bef_num_left == bef_num_right)
+            {
+                if (hand == "left")
+                {
+                    answer.append("L");
+                    bef_num_left = numbers[i];
+                }
+                else if (hand == "right")
+                {
+                    answer.append("R");
+                    bef_num_right = numbers[i];
+                }
+            }
+            else
+            {
+                if (numbers[i] == 0)
+                    numbers[i] == 11;
+
+                int diff_left = abs(bef_num_left - numbers[i]);
+                int diff_right = abs(bef_num_right - numbers[i]);
+                int dist_left = (diff_left / 3) + (diff_left % 3);
+                int dist_right = (diff_right / 3) + (diff_right % 3);
+
+                
+              //  cout << "dist_left : " << dist_left << " / dist_right : " << dist_right << endl;
+
+                if (dist_left == dist_right)
+                {
+                    if (hand == "left")
+                    {
+                        answer.append("L");
+                        bef_num_left = numbers[i];
+                    }
+                    else if (hand == "right")
+                    {
+                        answer.append("R");
+                        bef_num_right = numbers[i];
+                    }
+                }
+                else if (dist_left < dist_right)
+                {
+                    answer.append("L");
+                    bef_num_left = numbers[i];
+                }
+                else if (dist_left > dist_right)
+                {
+                    answer.append("R");
+                    bef_num_right = numbers[i];
+                }
+            }
+        }
+       // cout << "bef_left :" << bef_num_left << "/ bef_rigjt : " << bef_num_right << "/ i: " << i << endl;
+    }
+      
+    return answer;
+}
+int main()
+{
+    vector<int> sample1 = { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 };
+    string hand1 = "right";
+
+    cout << solution(sample1, hand1) << endl;
+
+    std::cout << "Hello World!\n";
+}
+
