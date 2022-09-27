5d461c1d7b328b10ae99c8be4fd0af394dfab391
Jaejun Lee <jaejun.lees@gmail.com>
2022-09-27 16:47:31 +0900

    프로그래머스, 백준 문제 풀이 추가

---
+++ baek_2178/baek_2178.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+using namespace std;
+#define max 101
+//int maze[max][max];
+bool isVisited[max][max] = { false };
+bool isRoad[max][max] = { false };
+int isCounter[max][max] = { 0 };
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+int counter = 0;
+int  N = 0;
+int M = 0;
+int bfs(int n, int m)
+{
+    queue<pair<int, int>> que;
+    que.push(make_pair(n, m));
+    isVisited[n][m] = true;
+    //  isCounter[n][m] = counter++;
+    while (!que.empty())
+    {
+        int n1 = que.front().first;
+        int m1 = que.front().second;
+        que.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int x1 = n1 + dx[i];
+            int y1 = m1 + dy[i];
+            if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < M)
+            {
+                //   cout << "i " << i << " x1 " << x1 << " y1 " << y1 << " isRoad " << endl;
+                if (isRoad[x1][y1] == true && isVisited[x1][y1] == false)
+                {
+                    // cout << " ok" << endl;
+                     //if ()
+
+                      //   cout << "pushed" << endl;
+                    //cout << "{ " << x1 << " " << y1 << " } " << counter << endl;
+
+                    isVisited[x1][y1] = true;
+                    isCounter[x1][y1] = isCounter[n1][m1] + 1;
+                    que.push(make_pair(x1, y1));
+                    //x2 = x1;
+                    //y2 = y1;
+                }
+            }
+        }
+    }
+    //cout << maxcounter + 1<< endl;
+    return 0;
+
+}
+int main()
+{
+    scanf_s("%d %d", &N, &M);
+    for (int i = 0; i < N; i++)
+    {
+        scanf_s("%s", isRoad[i]);
+    }
+    bfs(0, 0);
+    cout << isCounter[N - 1][M - 1] + 1 << endl;
+    //cout << bfs(0, 0) << endl;
+
+    //std::cout << "Hello World!\n";
+}
---
+++ solution_17680/solution_17680.cpp
@@ -0,0 +1,69 @@
+﻿#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    deque<string> cache;
+    deque<string>::iterator it;
+
+    for (int i = 0; i < cities.size(); i++)
+    {
+        for (int n = 0; n < cities[i].length(); n++)
+        {
+            if (cities[i][n] >= 65 && cities[i][n] <= 90)
+                cities[i][n] ^= 32;
+            //cout << cities[i][n] << endl;
+        }
+        auto it = find(cache.begin(), cache.end(), cities[i]);
+        if (it != cache.end())
+        {
+            //cache hit
+            answer += 1;
+            string ittemp = *it;
+            cache.erase(it);
+            cache.push_front(ittemp);
+            //    cout << ittemp << endl;
+        }
+        else if (cache.size() < cacheSize)
+        {
+            answer += 5;
+            //cache miss
+            cache.push_front(cities[i]);
+        }
+        else if (cache.size() == cacheSize)
+        {
+            //cache miss
+            answer += 5;
+            if (cacheSize >= 1)
+            {
+                cache.pop_back();
+                cache.push_front(cities[i]);
+            }
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<string> cities = { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome" };
+    int cacheSize = 5;
+    
+    std::cout << solution(cacheSize, cities) << endl;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
